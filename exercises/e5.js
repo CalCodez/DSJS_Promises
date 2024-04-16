a68501861efaf79c642be856a43b7d6dc958e5e2
calvin <CalCodez@outlook.com>
2024-04-15 22:33:18 -0500

    ex5 complete

--- exercises/e5.js
+++ exercises/e5.js
@@ -8,8 +8,8 @@
  *          export const Promise
  */
 
-export const attachTitle = () => {
-  // Your code goes here...
+export const attachTitle = (arg) => {
+  return 'DR. ' + arg;
 };
 
 /**
@@ -21,9 +21,12 @@ export const attachTitle = () => {
  */
 
 export const getPromise = () => {
-  // Your code goes here...
-};
+  return Promise.resolve('MANHATTAN')
 
+    .then(attachTitle)
+    .then(console.log);
+
+};
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
 // If the test has all tests passed, switch to the next exercise file
